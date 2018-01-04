755cce5dfe63af75eb968135d37314c8e17b24f6
gouf <innocent.zero@gmail.com>
2018-01-05 01:12:01 +0900

    Add seed data-set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#
+# Set seed data for working with example code
+#
+User.destroy_all
+
+10.times do
+  name = ('a'..'z').to_a.sample(8).join
+  User.create(name: name)
+end
+
+user = User.all
+
+%w[
+  2017-01-01
+  2017-01-02
+  2017-01-03
+  2017-01-04
+  2017-01-05
+  2017-01-06
+  2017-01-07
+  2017-01-08
+  2017-01-09
+  2017-01-10
+].each_with_index do |date, i|
+  user[i].update(updated_at: date)
+end
