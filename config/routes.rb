5044964996e44c9487a646bd165902a5f44bb6de
gouf <innocent.zero@gmail.com>
2018-01-05 06:57:36 +0900

    Implement user search form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/search', to: 'users#search'
   get 'users/search/:cwday', to: 'users#search'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
